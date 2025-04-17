015b4cd7df73da1833cccb919e7c11aa7fe9d580
Khaoula <rim.bakkali63@gmail.com>
2025-04-17 01:25:40 +0100

    adjust to cloud1 automation

--- src/requirements/MariaDB/conf/create.sh
+++ src/requirements/MariaDB/conf/create.sh
@@ -7,7 +7,11 @@ then
     mysql -e "CREATE USER '${DB_USER}'@'%' identified by '${DB_PASSWORD}';"
     mysql -e "GRANT ALL PRIVILEGES ON wordpress_database.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}' ;"
     mysql -e "flush privileges;"
-    mysql wordpress_database < wordpress.sql
+
+    # Replace placeholder IP in wordpress.sql with the value of INSTANCE_IP
+    sed "s/localhost/${INSTANCE_IP}/g" wordpress.sql  > temp_wordpress.sql
+    mysql wordpress_database <  temp_wordpress.sql
+
     #mysql -e "SET PASSWORD FOR 'root'@'localhost' = '2ff898e158cd0311';"
     #mysql -u root --password='root123'
     mysqladmin --user=root password ${MYSQL_ROOT_PASSWORD}
